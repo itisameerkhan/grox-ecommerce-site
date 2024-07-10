08c77481b2df31baba24a3af0e9e987bb9760e46
itisameerkhan <itisameerkhan@gmail.com>
2024-07-10 10:00:33 +0530

    add

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,38 @@
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer-main">
+      <div>
+        <p>About Us</p>
+        <p>Get to Know Us</p>
+        <p>Careers</p>
+        <p>Press Releases</p>
+      </div>
+      <div>
+        <p>Connect with Us</p>
+        <p>Facebook</p>
+        <p>Twitter</p>
+        <p>Instagram</p>
+      </div>
+      <div>
+        <p>Make Money with Us</p>
+        <p>Sell on Grox</p>
+        <p>Sell under Grox Accelerator</p>
+        <p>Protect and Build Your Brand</p>
+        <p>Become an Affiliate</p>
+        <p>Advertise Your Products</p>
+      </div>
+      <div>
+        <p>Let Us Help You</p>
+        <p>Your Account</p>
+        <p>Returns Centre</p>
+        <p>Recalls and Product Safety Alerts</p>
+        <p>100% Purchase Protection</p>
+        <p>Help</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import CardContainer from "../../components/CardContainer/CardContainer";
+import Footer from "../../components/Footer/Footer";
 import "./Home.scss";
 
 const Home = () => {
   return (
     <div className="home">
       <CardContainer />
+      <Footer />
     </div>
   );
 };
