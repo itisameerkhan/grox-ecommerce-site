4332b49f0237c9ac6e68b82f0fee29ce57555ad2
itisameerkhan <itisameerkhan@gmail.com>
2024-07-10 12:55:06 +0530

    add

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -2,34 +2,29 @@ import "./Spinner.scss";
 
 const Spinner = () => {
   return (
-    <div className="loader">
-      <div className="boxes">
-        <div className="box">
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+    <div className="loader-main">
+      <div className="loader">
+        <div className="box box-1">
+          <div className="side-left"></div>
+          <div className="side-right"></div>
+          <div className="side-top"></div>
         </div>
-        <div className="box">
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+        <div className="box box-2">
+          <div className="side-left"></div>
+          <div className="side-right"></div>
+          <div className="side-top"></div>
         </div>
-        <div className="box">
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+        <div className="box box-3">
+          <div className="side-left"></div>
+          <div className="side-right"></div>
+          <div className="side-top"></div>
         </div>
-        <div className="box">
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+        <div className="box box-4">
+          <div className="side-left"></div>
+          <div className="side-right"></div>
+          <div className="side-top"></div>
         </div>
       </div>
-
       <p>Please wait we are contacting our servers.</p>
     </div>
   );
